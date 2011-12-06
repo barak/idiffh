018ea98b8e4d61a6516684405121474eef36a4d2
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2011-12-06 19:48:45 +0000

    tweak separate to use stdin/stdout, other boilerplate

--- separate.c
+++ separate.c
@@ -1,153 +1,159 @@
-
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 
+// tokenises standard input, emitting one line/token, with trailing
+// whitespace (possibly empty) following each token represented by a
+// colon followed by percent-encoded ascii.
+
+// Example, input:
+
+//   The quick, brown
+// fox  jumps! over
+
+// generated output:
+
+// :%32%32
+// The:%32
+// quick:
+// ,:%32
+// brown:%13
+// fox:%32%32
+// jumps:
+// !:%32
+// over:%13
+
+// Note: compile with -DNDEBUG to turn off debugging chatter.
+
+void
+usage()
+{
+  fprintf(stderr, "Tokenize stdin, stdout is one token/line, whitespace encoded.\n");
+  fprintf(stderr, "Usage: separate < file-to-tokenize > token-file\n");
+  fprintf(stderr, "Example: (echo a man, a; echo plan)|separate\n");
+}
+
+
+int
 main (int argc, char *argv[])
 {
-  int x = 0;
-  while (x < 3)
+  if (argc != 1)
     {
+      usage();
+      exit(1);
+    }
 
-      FILE *original;
-      FILE *words;
-      char ch;
-      original = fopen (argv[x + 1], "r");
-      if (x == 0)
+  char temp;
+  int i = 0;
+  int ch;
+
+  while ((ch = getchar ()) != EOF)
+    {
+#ifndef NDEBUG
+      fprintf (stderr, "(%c)", ch);
+#endif
+      if (ch == '\t' && (isalpha (temp)))
 	{
-	  words = fopen ("/home/user/words3", "w");
+	  puts (": ");
+	  puts ("%9");
 	}
-      if (x == 1)
+      else if (i == 0 && (isalpha (ch)))
 	{
-	  words = fopen ("/home/user/words4", "w");
+	  putchar (ch);
 	}
-      if (x == 2)
+      else if ((isalpha (temp)) && ch == '\n')
 	{
-	  words = fopen ("/home/user/words5", "w");
+	  puts (": ");
+	  puts ("%13");
+	  putchar ('\n');
+
 	}
-      fseek (original, 0, SEEK_END);	// seek to end of file
-      int size = ftell (original);	// get current file pointer
-      fseek (original, 0, SEEK_SET);
-      char temp;
-      int i = 0;
-
-      //need to add extra loop for the amount of files.
-      while (i < size)		// Need to change to lenght of file!
+      else if (!(isalpha (temp)) && ch == '\n')
 	{
-	  ch = fgetc (original);
-	  printf ("%c", ch);
-	  if (ch == EOF)
-	    {
-	      break;
-	    }
-	  else if (ch == '\t' && (isalpha (temp)))
-	    {
-	      fputs (": ", words);
-	      fputs ("%9", words);
-	    }
-	  else if (i == 0 && (isalpha (ch)))
-	    {
-	      fputc (ch, words);
-	    }
-	  else if ((isalpha (temp)) && ch == '\n')
-	    {
-	      fputs (": ", words);
-	      fputs ("%13", words);
-	      fputc ('\n', words);
+	  puts (": ");
+	  puts ("%13");
+	  putchar ('\n');
 
-	    }
-	  else if (!(isalpha (temp)) && ch == '\n')
-	    {
-	      fputs (": ", words);
-	      fputs ("%13", words);
-	      fputc ('\n', words);
+	}
+      else if (temp == ' ' && ch == '\n')
+	{
+	  puts ("%13");
+	  putchar ('\n');
 
-	    }
-	  else if (temp == ' ' && ch == '\n')
-	    {
-	      fputs ("%13", words);
-	      fputc ('\n', words);
+	}
+      else if (ch == '\n')
+	{
+	  puts (": ");
+	  puts ("%13");
+	  putchar ('\n');
 
-	    }
-	  else if (ch == '\n')
-	    {
-	      fputs (": ", words);
-	      fputs ("%13", words);
-	      fputc ('\n', words);
-
-	    }
-	  else if (temp == '\n' && isalpha (ch))
-	    {
-	      fputc (ch, words);
-	    }
-	  else if (temp == '\n' && (!isalpha (ch)))
-	    {
-	      fputc (ch, words);
-	    }
-	  else if (temp == ' ' && (isalpha (ch)))
-	    {
-	      fputc ('\n', words);
-	      fputc (ch, words);
-	    }
-
-	  else if ((!(isalpha (temp))) && (isalpha (ch)))
-	    {
-	      fputc ('\n', words);
-	      fputc (ch, words);
-	    }
-	  else if (isalpha (ch))
-	    {
-	      fputc (ch, words);
-	    }
-
-	  else if ((isalpha (temp)) && ch == ' ')
-	    {
-	      fputs (": ", words);
-	      fputs ("%32", words);
-
-	    }
-	  else if ((!(isalpha (temp))) && temp != ' ' && ch == ' ')
-	    {
-	      fputs (": ", words);
-	      fputs ("%32", words);
-	    }
-	  else if (ch == ' ')
-	    {
-	      fputs ("%32", words);
-	    }
-	  else if ((!(isalpha (ch))) && temp == ' ')
-	    {
-	      fputc ('\n', words);
-	      fputc (ch, words);
-
-
-	    }
-
-	  else if ((isalpha (temp)) && (!(isalpha (ch))))
-	    {
-	      fputc ('\n', words);
-	      fputc (ch, words);
-
-	    }
-	  else if (!(isalpha (temp)) && (!(isalpha (ch))))
-	    {
-	      fputc ('\n', words);
-	      fputc (ch, words);
-
-	    }
-
-	  else
-	    {
-	      fputc (ch, words);
-	    }
-
-	  temp = ch;
-	  i++;
+	}
+      else if (temp == '\n' && isalpha (ch))
+	{
+	  putchar (ch);
+	}
+      else if (temp == '\n' && (!isalpha (ch)))
+	{
+	  putchar (ch);
+	}
+      else if (temp == ' ' && (isalpha (ch)))
+	{
+	  putchar ('\n');
+	  putchar (ch);
 	}
 
+      else if ((!(isalpha (temp))) && (isalpha (ch)))
+	{
+	  putchar ('\n');
+	  putchar (ch);
+	}
+      else if (isalpha (ch))
+	{
+	  putchar (ch);
+	}
+
+      else if ((isalpha (temp)) && ch == ' ')
+	{
+	  puts (": ");
+	  puts ("%32");
+
+	}
+      else if ((!(isalpha (temp))) && temp != ' ' && ch == ' ')
+	{
+	  puts (": ");
+	  puts ("%32");
+	}
+      else if (ch == ' ')
+	{
+	  puts ("%32");
+	}
+      else if ((!(isalpha (ch))) && temp == ' ')
+	{
+	  putchar ('\n');
+	  putchar (ch);
 
 
-      fclose (original);
-      fclose (words);
-      x++;
+	}
+
+      else if ((isalpha (temp)) && (!(isalpha (ch))))
+	{
+	  putchar ('\n');
+	  putchar (ch);
+
+	}
+      else if (!(isalpha (temp)) && (!(isalpha (ch))))
+	{
+	  putchar ('\n');
+	  putchar (ch);
+
+	}
+
+      else
+	{
+	  putchar (ch);
+	}
+
+      temp = ch;
+      i++;
     }
 }
