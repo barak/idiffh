2e417c7be95983c3dad6cca27b54cadcd138a170
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2011-12-06 19:22:49 +0000

    indent separate.c

--- separate.c
+++ separate.c
@@ -2,129 +2,152 @@
 #include <stdio.h>
 #include <ctype.h>
 
-main(int argc, char *argv[]) {
-int x = 0;
-while (x < 3){
- 		 
-	FILE *original;
-	FILE *words;
-	 char ch ;
-	original = fopen(argv[x+1], "r"); 
-        if(x == 0){
-		words = fopen("/home/user/words3", "w");
+main (int argc, char *argv[])
+{
+  int x = 0;
+  while (x < 3)
+    {
+
+      FILE *original;
+      FILE *words;
+      char ch;
+      original = fopen (argv[x + 1], "r");
+      if (x == 0)
+	{
+	  words = fopen ("/home/user/words3", "w");
 	}
-	if(x == 1){
-		words = fopen("/home/user/words4", "w");
+      if (x == 1)
+	{
+	  words = fopen ("/home/user/words4", "w");
 	}
-	if(x == 2){	
-		words = fopen("/home/user/words5", "w");
+      if (x == 2)
+	{
+	  words = fopen ("/home/user/words5", "w");
 	}
-fseek(original, 0, SEEK_END); // seek to end of file
-int size = ftell(original); // get current file pointer
-fseek(original, 0, SEEK_SET);
-char temp;
-int i = 0;
-		
-	//need to add extra loop for the amount of files.
-	  while ( i<size )// Need to change to lenght of file!
-	        {
-	             ch= fgetc ( original ) ;
-			printf("%c", ch);
-	               if ( ch == EOF ){
-	                      break ;
-			}
-			else if(ch == '\t' && (isalpha(temp))){
-				fputs(": ", words);
-				fputs("%9", words);
-			}
-			else if(i == 0 && (isalpha(ch))){
-				fputc(ch, words);
-				}
-			else if((isalpha(temp)) && ch == '\n'){
-				fputs(": ", words);
-				fputs("%13", words);
-				fputc('\n', words);
-				
-			}
-			else if(!(isalpha(temp)) && ch == '\n'){
-				fputs(": ", words);
-				fputs("%13", words);
-				fputc('\n', words);
-				
-			}
-			else if(temp == ' ' && ch == '\n'){
-				fputs("%13", words);
-				fputc('\n', words);
-				
-			}
-			else if(ch == '\n'){
-				fputs(": ", words);
-				fputs("%13", words);
-				fputc('\n', words);
-				
-			}
-			else if(temp == '\n' && isalpha(ch)){
-				fputc(ch, words);
-			}
-			else if(temp == '\n' && (!isalpha(ch))){
-				fputc(ch, words);
-			}
-	               else if(temp == ' ' && (isalpha(ch))){
-	            	 	fputc('\n', words);
-	            		fputc(ch, words);
-	               }
-			
-		       else if((!(isalpha(temp))) && (isalpha(ch))){
-				fputc ('\n', words);
-				fputc (ch, words);
- 		       }
-	               else if(isalpha(ch)){
-	            		fputc(ch, words);
-	               }
+      fseek (original, 0, SEEK_END);	// seek to end of file
+      int size = ftell (original);	// get current file pointer
+      fseek (original, 0, SEEK_SET);
+      char temp;
+      int i = 0;
 
-	               else if((isalpha(temp)) && ch == ' '){           	   
-			   fputs(": " , words);
-                           fputs("%32", words);
-				
-	               }
-			else if((!(isalpha(temp))) && temp != ' ' && ch == ' '){
-				 fputs(": " , words);
-                          	 fputs("%32", words);
-				}
-                       else if (ch == ' '){
-                	fputs("%32", words);
-                       }
-		       else if((!(isalpha(ch))) && temp == ' '){
-			  fputc ('\n', words);
-			  fputc ( ch, words );
-			   
+      //need to add extra loop for the amount of files.
+      while (i < size)		// Need to change to lenght of file!
+	{
+	  ch = fgetc (original);
+	  printf ("%c", ch);
+	  if (ch == EOF)
+	    {
+	      break;
+	    }
+	  else if (ch == '\t' && (isalpha (temp)))
+	    {
+	      fputs (": ", words);
+	      fputs ("%9", words);
+	    }
+	  else if (i == 0 && (isalpha (ch)))
+	    {
+	      fputc (ch, words);
+	    }
+	  else if ((isalpha (temp)) && ch == '\n')
+	    {
+	      fputs (": ", words);
+	      fputs ("%13", words);
+	      fputc ('\n', words);
 
-		       }
-		      
-		       else if((isalpha(temp)) && (!(isalpha(ch)))){
-			fputc ('\n', words);
-			fputc ( ch, words );
-	            	 
- 		       }
-		       else if(!(isalpha(temp)) && (!(isalpha(ch)))){
-			fputc ('\n', words);
-			fputc ( ch, words );
-	            	 
- 		       }
-			
-                       else{
-	            	   fputc ( ch, words );
-	               }
+	    }
+	  else if (!(isalpha (temp)) && ch == '\n')
+	    {
+	      fputs (": ", words);
+	      fputs ("%13", words);
+	      fputc ('\n', words);
 
-	        temp = ch;
-		i++;
-	        }
-		
+	    }
+	  else if (temp == ' ' && ch == '\n')
+	    {
+	      fputs ("%13", words);
+	      fputc ('\n', words);
+
+	    }
+	  else if (ch == '\n')
+	    {
+	      fputs (": ", words);
+	      fputs ("%13", words);
+	      fputc ('\n', words);
+
+	    }
+	  else if (temp == '\n' && isalpha (ch))
+	    {
+	      fputc (ch, words);
+	    }
+	  else if (temp == '\n' && (!isalpha (ch)))
+	    {
+	      fputc (ch, words);
+	    }
+	  else if (temp == ' ' && (isalpha (ch)))
+	    {
+	      fputc ('\n', words);
+	      fputc (ch, words);
+	    }
+
+	  else if ((!(isalpha (temp))) && (isalpha (ch)))
+	    {
+	      fputc ('\n', words);
+	      fputc (ch, words);
+	    }
+	  else if (isalpha (ch))
+	    {
+	      fputc (ch, words);
+	    }
+
+	  else if ((isalpha (temp)) && ch == ' ')
+	    {
+	      fputs (": ", words);
+	      fputs ("%32", words);
+
+	    }
+	  else if ((!(isalpha (temp))) && temp != ' ' && ch == ' ')
+	    {
+	      fputs (": ", words);
+	      fputs ("%32", words);
+	    }
+	  else if (ch == ' ')
+	    {
+	      fputs ("%32", words);
+	    }
+	  else if ((!(isalpha (ch))) && temp == ' ')
+	    {
+	      fputc ('\n', words);
+	      fputc (ch, words);
 
 
-	        fclose ( original ) ;
-            fclose(words);
-		x++;
-		}
+	    }
+
+	  else if ((isalpha (temp)) && (!(isalpha (ch))))
+	    {
+	      fputc ('\n', words);
+	      fputc (ch, words);
+
+	    }
+	  else if (!(isalpha (temp)) && (!(isalpha (ch))))
+	    {
+	      fputc ('\n', words);
+	      fputc (ch, words);
+
+	    }
+
+	  else
+	    {
+	      fputc (ch, words);
+	    }
+
+	  temp = ch;
+	  i++;
+	}
+
+
+
+      fclose (original);
+      fclose (words);
+      x++;
+    }
 }
-
